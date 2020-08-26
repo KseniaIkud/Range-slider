e82f82c91fb8b59992d987d989dcea2c25342460
Ksenia <kudriavtseva_k@mail.ru>
2020-08-26 10:08:14 +0300

    code refactoring

--- dist/double-range.cfc12fc5.js
+++ dist/double-range.cfc12fc5.js
@@ -117,83 +117,93 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
-var bundleURL = null;
+})({"js/double-range.js":[function(require,module,exports) {
+jQuery(function () {
+  //isRange = true
+  var inputLeft = document.getElementById("input-left");
+  var inputRight = document.getElementById("input-right");
+  var thumbLeft = document.querySelector(".js-range-slider__thumb-left");
+  var thumbRight = document.querySelector(".js-range-slider__thumb-right");
+  var range = document.querySelector(".js-range-slider__progress-bar");
 
-function getBundleURLCached() {
-  if (!bundleURL) {
-    bundleURL = getBundleURL();
+  function calcPercentBar(value, min, max) {
+    var percent = (value - min) / (max - min) * 100;
+    return percent;
   }
 
-  return bundleURL;
-}
-
-function getBundleURL() {
-  // Attempt to find the URL of the current script and use that as the base URL
-  try {
-    throw new Error();
-  } catch (err) {
-    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
-
-    if (matches) {
-      return getBaseURL(matches[0]);
-    }
+  function calcLeftValue(left, right) {
+    var result = Math.min(parseInt(left), parseInt(right) - 1);
+    return result;
   }
 
-  return '/';
-}
-
-function getBaseURL(url) {
-  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)\/[^/]+$/, '$1') + '/';
-}
-
-exports.getBundleURL = getBundleURLCached;
-exports.getBaseURL = getBaseURL;
-},{}],"../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
-var bundle = require('./bundle-url');
-
-function updateLink(link) {
-  var newLink = link.cloneNode();
-
-  newLink.onload = function () {
-    link.remove();
-  };
-
-  newLink.href = link.href.split('?')[0] + '?' + Date.now();
-  link.parentNode.insertBefore(newLink, link.nextSibling);
-}
-
-var cssTimeout = null;
-
-function reloadCSS() {
-  if (cssTimeout) {
-    return;
+  function setLeftValue() {
+    var min = parseInt(inputLeft.min),
+        max = parseInt(inputLeft.max);
+    inputLeft.value = calcLeftValue(inputLeft.value, inputRight.value);
+    var percent = calcPercentBar(inputLeft.value, min, max);
+    thumbLeft.style.left = percent + "%";
+    range.style.left = percent + "%";
   }
 
-  cssTimeout = setTimeout(function () {
-    var links = document.querySelectorAll('link[rel="stylesheet"]');
+  function calcRightValue(left, right) {
+    var result = Math.max(parseInt(right), parseInt(left) + 1);
+    return result;
+  }
 
-    for (var i = 0; i < links.length; i++) {
-      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
-        updateLink(links[i]);
-      }
-    }
+  function setRightValue() {
+    var min = parseInt(inputRight.min),
+        max = parseInt(inputRight.max);
+    inputRight.value = calcRightValue(inputLeft.value, inputRight.value);
+    var percent = calcPercentBar(inputRight.value, min, max);
+    thumbRight.style.right = 100 - percent + "%";
+    range.style.right = 100 - percent + "%";
+  }
 
-    cssTimeout = null;
-  }, 50);
-}
+  inputLeft.addEventListener("input", setLeftValue);
+  inputRight.addEventListener("input", setRightValue); //isOutput = true
 
-module.exports = reloadCSS;
-},{"./bundle-url":"../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"main.scss":[function(require,module,exports) {
-var reloadCSS = require('_css_loader');
+  var outputLeft = document.querySelector(".js-output-data_left");
+  var outputRight = document.querySelector(".js-output-data_right");
 
-module.hot.dispose(reloadCSS);
-module.hot.accept(reloadCSS);
-},{"_css_loader":"../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"main.js":[function(require,module,exports) {
-"use strict";
+  function getLeftValue() {
+    outputLeft.value = inputLeft.value;
+  }
 
-require("./main.scss");
-},{"./main.scss":"main.scss"}],"../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+  function getRightValue() {
+    outputRight.value = inputRight.value;
+  }
+
+  getLeftValue();
+  getRightValue();
+
+  function showLeftValue() {
+    var _this = outputLeft,
+        min = parseInt(inputLeft.min),
+        max = parseInt(inputLeft.max);
+    var percent = (_this.value - min) / (max - min) * 100;
+    thumbLeft.style.left = percent + "%";
+    range.style.left = percent + "%";
+    inputLeft.value = _this.value;
+    outputLeft.max = parseInt(inputRight.value) - 1;
+  }
+
+  function showRightValue() {
+    var _this = outputRight,
+        min = parseInt(inputRight.min),
+        max = parseInt(inputRight.max);
+    var percent = (_this.value - min) / (max - min) * 100;
+    thumbRight.style.right = 100 - percent + "%";
+    range.style.right = 100 - percent + "%";
+    inputRight.value = _this.value;
+    outputRight.min = parseInt(inputLeft.value) + 1;
+  }
+
+  inputLeft.addEventListener("input", getLeftValue);
+  inputRight.addEventListener("input", getRightValue);
+  outputLeft.addEventListener("input", showLeftValue);
+  outputRight.addEventListener("input", showRightValue);
+});
+},{}],"../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -397,5 +407,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","main.js"], null)
-//# sourceMappingURL=/main.1f19ae8e.js.map
+},{}]},{},["../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/double-range.js"], null)
+//# sourceMappingURL=/double-range.cfc12fc5.js.map
