0b531d03ad9b29a70a567c00300ddefcb7438a9b
Ksenia <kudriavtseva_k@mail.ru>
2020-09-01 23:52:02 +0300

    webpack config created

---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+
+import './main.scss'
+
+import './js/single-range'
+
+console.log('index')
--- src/js/single-range.js
+++ src/js/single-range.js
@@ -1,3 +1,4 @@
+
 jQuery(function() {
 
     let rangeSlider = document.querySelector(".range-slider");
