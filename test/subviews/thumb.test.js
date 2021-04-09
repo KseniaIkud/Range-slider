cbff212a5ff443f8b0ba86d8196741ab61e66672
Ksenia <kudriavtseva_k@mail.ru>
2021-04-09 07:56:01 +0300

    thumb tests completed

--- test/subviews/thumb.test.js
+++ test/subviews/thumb.test.js
@@ -2,3 +2,40 @@ import {Thumb} from '../../src/mvc/subViews'
 
 const _ = new Thumb()
 
+describe('setThumbValue', () => {
+    test('should not do anything', () => {
+        _.setThumbValue(false, 2)
+        expect(_.thumbOutput).toBeUndefined
+    })
+    test('thumb value should be 50', () => {
+        _.createThumbElement(false, document.body)
+        _.setThumbValue(false, 50)
+        expect(_.thumbOutput.textContent).toBe('50')
+    })
+    test('thumb values should be -200 and 0', () => {
+        _.createThumbElement(true, document.body, document.body)
+        _.setThumbValue(true, -200, 0)
+        expect(_.thumbOutput.textContent).toBe('-200')
+        expect(_.thumbOutputRight.textContent).toBe('0')
+    })
+})
+
+describe('createThumbElement', () => {
+    test('one thumb should be created', () => {
+        let createElementMock = jest.spyOn(document, "createElement")
+        jest.clearAllMocks()
+        _.createThumbElement(false, document.body)
+        expect(document.createElement).toHaveBeenCalledTimes(1);
+        expect(_.thumbOutput.classList.contains('range-slider__value-thumb'))
+        createElementMock.mockRestore()
+    })
+    test('two thumbs should be created', () => {
+        let createElementMock = jest.spyOn(document, "createElement")
+        jest.clearAllMocks()
+        _.createThumbElement(true, document.body, document.body)
+        expect(document.createElement).toHaveBeenCalledTimes(2);
+        expect(_.thumbOutput.classList.contains('range-slider__value-thumb'))
+        expect(_.thumbOutputRight.classList.contains('range-slider__value-thumb'))
+        createElementMock.mockRestore()
+    })
+})
