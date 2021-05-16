9f89f0d7a2adf3439dbe433a5e0e2e32a4715b4c
Ksenia <kudriavtseva_k@mail.ru>
2021-05-16 13:24:18 +0300

    webpack config fixed for eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,141 +1,139 @@
-const path = require('path')
-const HTMLWebpackPlugin = require("html-webpack-plugin")
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
-const TerserWebpackPlugin = require('terser-webpack-plugin')
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
 
-const isDev = process.env.NODE_ENV === 'development'
-const isProd = !isDev
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
 
 const optimization = () => {
-    const config = {
-    }
+  const config = {
+  };
 
-    if(isProd) {
-        config.minimizer = [
-            new OptimizeCssAssetsWebpackPlugin(),
-            new TerserWebpackPlugin(),
-        ]
-    }
+  if (isProd) {
+    config.minimizer = [
+      new OptimizeCssAssetsWebpackPlugin(),
+      new TerserWebpackPlugin(),
+    ];
+  }
 
-    return config
-}
+  return config;
+};
 
 const cssLoaders = (extra) => {
-    const loaders = [
+  const loaders = [
     {
-        loader: MiniCssExtractPlugin.loader,
-        options: {
-            hmr: isDev,
-            reloadAll: true  
-        },
-    }, 
-    'css-loader']
+      loader: MiniCssExtractPlugin.loader,
+      options: {
+        hmr: isDev,
+        reloadAll: true,
+      },
+    },
+    'css-loader'];
 
-    if (extra) {
-        loaders.push(extra)
-    }
+  if (extra) {
+    loaders.push(extra);
+  }
 
-    return loaders
-}
+  return loaders;
+};
 
-const babelOptions = preset => {
-    const options = {
-        presets: [
-            '@babel/preset-env'
-        ],
-        plugins: [
-            '@babel/plugin-proposal-class-properties'
-        ]
-    }
-    if (preset) {
-        options.presets.push(preset)
-    }
-    return options
-}
+const babelOptions = (preset) => {
+  const options = {
+    presets: [
+      '@babel/preset-env',
+    ],
+    plugins: [
+      '@babel/plugin-proposal-class-properties',
+    ],
+  };
+  if (preset) {
+    options.presets.push(preset);
+  }
+  return options;
+};
 
 const jsLoaders = () => {
-    const loaders = [{
-        loader: 'babel-loader',
-        options: babelOptions()
-    }]
+  const loaders = [{
+    loader: 'babel-loader',
+    options: babelOptions(),
+  }];
 
-    if (isDev) {
-        loaders.push('eslint-loader')
-    }
-    return loaders
-}
+  if (isDev) {
+    loaders.push('eslint-loader');
+  }
+  return loaders;
+};
 
 const plugins = () => {
+  const base = [
+    new HTMLWebpackPlugin({
+      template: './index.html',
+      minify: {
+        collapseWhitespace: isProd,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
 
-    const base = [
-        new HTMLWebpackPlugin({
-            template: './index.html',
-            minify: {
-                collapseWhitespace: isProd
-            }
-        }),
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            
-        })
-    ]
+    }),
+  ];
 
-
-    return base
-}
+  return base;
+};
 
 module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    entry: {
-        rangeSlider: ['@babel/polyfill','./rangeSlider.js'],
-        index: ['@babel/polyfill', './demo/demo.js']
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'docs')
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    optimization: optimization(),
-    devtool: isDev ? 'inline-source-map' : '',
-    plugins: plugins(),
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: cssLoaders()
-            }, {
-                test: /\.s[ac]ss$/,
-                use: cssLoaders('sass-loader')
-            }, {
-                test: /\.(png|jpg|svg|gif)$/,
-                use: ['file-loader']
-            },
-            {
-                test: /\.(ttf|woff|woff2|eot)$/,
-                use: ['file-loader']
-            }, {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: jsLoaders()
-            },
-            {
-                test: /\.ts$/,
-                exclude: /node_modules/,
-                loader: {
-                    loader: 'babel-loader',
-                    options: babelOptions('@babel/preset-typescript')
-                }
-            }
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    rangeSlider: ['@babel/polyfill', './rangeSlider.js'],
+    index: ['@babel/polyfill', './demo/demo.js'],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'docs'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  optimization: optimization(),
+  devtool: isDev ? 'inline-source-map' : '',
+  plugins: plugins(),
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: cssLoaders(),
+      }, {
+        test: /\.s[ac]ss$/,
+        use: cssLoaders('sass-loader'),
+      }, {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        use: ['file-loader'],
+      }, {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: jsLoaders(),
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: {
+          loader: 'babel-loader',
+          options: babelOptions('@babel/preset-typescript'),
+        },
+      },
 
-        ]
-    },
-    devServer: {
-        port: 1234,
-        hot: isDev
-    }
-}
+    ],
+  },
+  devServer: {
+    port: 1234,
+    hot: isDev,
+  },
+};
