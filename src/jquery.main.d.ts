bbfeb3c0847eab2c6f85c88852d24b00504a359c
Ksenia <kudriavtseva_k@mail.ru>
2021-05-16 13:22:34 +0300

    main.d.ts file fixed for eslint

--- src/jquery.main.d.ts
+++ src/jquery.main.d.ts
@@ -1,49 +1,47 @@
+import { Slider } from './mvc/view/slider';
+import { Track } from './mvc/view/track';
+import { ProgressBar } from './mvc/view/progressBar';
+import { Thumb } from './mvc/view/thumb';
+import { Scale } from './mvc/view/scale';
+import { View } from './mvc/view/view';
+import { Model } from './mvc/model';
+import { Controller } from './mvc/controller';
 
-import {Slider} from './mvc/view/slider'
-import {Track} from './mvc/view/track'
-import {ProgressBar} from './mvc/view/progressBar'
-import {Thumb} from './mvc/view/thumb'
-import {Scale} from './mvc/view/scale'
-import {View} from './mvc/view/view'
-import {Model} from './mvc/model'
-import {Controller} from './mvc/controller'
-
-
-(function($: JQueryStatic) {
-    $.fn.rangeSlider = function(settings: {
-        min?: number
-        max?: number
-        initialValue?: number
-        leftValue?: number
-        rightValue?: number
-        isRange?: boolean
-        rightProgressBar?: boolean
-        overThumbElement?: boolean
-        step?: number
-        isVertical?: boolean
-        isScale?: boolean
-    }) {
-        return new Controller(
-            new Model({
-                min: settings.min,
-                max: settings.max,
-                defaultValue: settings.isRange ? settings.leftValue : settings.initialValue,
-                rightValue: settings.rightValue,
-                isRange: settings.isRange,
-                rightProgressBar: settings.rightProgressBar,
-                overThumbElement: settings.overThumbElement,
-                step: settings.step,
-                isVertical: settings.isVertical,
-                isScale: settings.isScale
-            }), 
-            new View( 
-                this,
-                new Slider(),
-                new Track(),
-                new ProgressBar(),
-                new Thumb(),
-                new Scale()
-            )
-        )
-    }
-})(jQuery)
+(function initSlider($: JQueryStatic) {
+  $.fn.rangeSlider = function init(settings: {
+    min?: number
+    max?: number
+    initialValue?: number
+    leftValue?: number
+    rightValue?: number
+    isRange?: boolean
+    rightProgressBar?: boolean
+    overThumbElement?: boolean
+    step?: number
+    isVertical?: boolean
+    isScale?: boolean
+  }) {
+    return new Controller(
+      new Model({
+        min: settings.min,
+        max: settings.max,
+        defaultValue: settings.isRange ? settings.leftValue : settings.initialValue,
+        rightValue: settings.rightValue,
+        isRange: settings.isRange,
+        rightProgressBar: settings.rightProgressBar,
+        overThumbElement: settings.overThumbElement,
+        step: settings.step,
+        isVertical: settings.isVertical,
+        isScale: settings.isScale,
+      }),
+      new View(
+        this,
+        new Slider(),
+        new Track(),
+        new ProgressBar(),
+        new Thumb(),
+        new Scale(),
+      ),
+    );
+  };
+}(jQuery));
